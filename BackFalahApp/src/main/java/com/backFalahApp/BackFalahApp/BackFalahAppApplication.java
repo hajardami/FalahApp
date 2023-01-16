1204e636df5dd66b02602930768d1718a7e8a6e0
Hajar <hajar.dami.14@gmail.com>
2023-01-16 13:18:31 +0100

    first commit

---
+++ BackFalahApp/src/main/java/com/backFalahApp/BackFalahApp/BackFalahAppApplication.java
@@ -0,0 +1,13 @@
+package com.backFalahApp.BackFalahApp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BackFalahAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BackFalahAppApplication.class, args);
+	}
+
+}
---
+++ BackFalahApp/src/test/java/com/backFalahApp/BackFalahApp/BackFalahAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.backFalahApp.BackFalahApp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackFalahAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
